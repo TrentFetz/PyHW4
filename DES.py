b75dd04b289e7d32b6d18f33b29fcf762d46b5b9
Trent Fetzer <118493986+TrentFetz@users.noreply.github.com>
2024-04-16 20:16:59 -0400

    Update DES.py

--- DES.py
+++ DES.py
@@ -1,143 +1,181 @@
 import random 
 import time
 
-
 ef = [
-	32,	1,	2,	3,	4,	5,
-	4,	5,	6,	7,	8,	9,
-	8,	9,	10,	11,	12,	13,
-	12,	13,	14,	15,	16,	17,
-	16,	17,	18,	19,	20,	21,
-	20,	21,	22,	23,	24,	25,
-	24,	25,	26,	27,	28,	29,
-	28,	29,	30,	31,	32,	1
+    32,	1,	2,	3,	4,	5,
+    4,	5,	6,	7,	8,	9,
+    8,	9,	10,	11,	12,	13,
+    12,	13,	14,	15,	16,	17,
+    16,	17,	18,	19,	20,	21,
+    20,	21,	22,	23,	24,	25,
+    24,	25,	26,	27,	28,	29,
+    28,	29,	30,	31,	32,	1
 ]
 ip = [
-	58, 50, 42, 34, 26, 18, 10, 2,
-	60,	52,	44,	36,	28,	20,	12,	4,
-	62,	54,	46,	38,	30,	22,	14,	6,
-	64,	56,	48,	40,	32,	24,	16,	8,
-	57,	49,	41,	33,	25,	17,	9,  1,
-	59,	51,	43,	35,	27,	19,	11,	3,
-	61,	53,	45,	37,	29,	21,	13,	5,
-	63,	55,	47,	39,	31,	23,	15,	7
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60,	52,	44,	36,	28,	20,	12,	4,
+    62,	54,	46,	38,	30,	22,	14,	6,
+    64,	56,	48,	40,	32,	24,	16,	8,
+    57,	49,	41,	33,	25,	17,	9,  1,
+    59,	51,	43,	35,	27,	19,	11,	3,
+    61,	53,	45,	37,	29,	21,	13,	5,
+    63,	55,	47,	39,	31,	23,	15,	7
 ]
 pc2 = [
-	14, 17, 11, 24, 1, 5,
-	3, 28, 15, 6, 21, 10,
-	23, 19, 12, 4, 26, 8,
-	16, 7, 27, 20, 13, 2,
-	41, 52, 31, 37, 47, 55,
-	30, 40, 51, 45, 33, 48,
-	44, 49, 39, 56, 34, 53,
-	46, 42, 50, 36, 29, 32
+    14, 17, 11, 24, 1, 5,
+    3, 28, 15, 6, 21, 10,
+    23, 19, 12, 4, 26, 8,
+    16, 7, 27, 20, 13, 2,
+    41, 52, 31, 37, 47, 55,
+    30, 40, 51, 45, 33, 48,
+    44, 49, 39, 56, 34, 53,
+    46, 42, 50, 36, 29, 32
 ]
 fp = [
-	40,	8,	48,	16,	56,	24,	64,	32,
-	39,	7,	47,	15,	55,	23,	63,	31,
-	38,	6,	46,	14,	54,	22,	62,	30,
-	37,	5,	45,	13,	53,	21,	61,	29,
-	36,	4,	44,	12,	52,	20,	60,	28,
-	35,	3,	43,	11,	51,	19,	59,	27,
-	34,	2,	42,	10,	50,	18,	58,	26,
-	33,	1,	41,	9,  49,	17,	57,	25
+    40,	8,	48,	16,	56,	24,	64,	32,
+    39,	7,	47,	15,	55,	23,	63,	31,
+    38,	6,	46,	14,	54,	22,	62,	30,
+    37,	5,	45,	13,	53,	21,	61,	29,
+    36,	4,	44,	12,	52,	20,	60,	28,
+    35,	3,	43,	11,	51,	19,	59,	27,
+    34,	2,	42,	10,	50,	18,	58,	26,
+    33,	1,	41,	9,  49,	17,	57,	25
+]
+SBox = [
+    [[14,	4,	13,	1,	2,	15,	11,	8,	3,	10,	6,	12,	5,	9,	0,	7],
+	[0,	15,	7,	4,	14,	2,	13,	1,	10,	6,	12,	11,	9,	5,	3,	8],
+	[4,	1,	14,	8,	13,	6,	2,	11,	15,	12,	9,	7,	3,	10,	5,	0],
+	[15,	12,	8,	2,	4,	9,	1,	7,	5,	11,	3,	14,	10,	0,	6,	13],
+    ],
+
+    [[15,	1,	8,	14,	6,	11,	3,	4,	9,	7,	2,	13,	12,	0,	5,	10],
+    [3,	13,	4,	7,	15,	2,	8,	14,	12,	0,	1,	10,	6,	9,	11,	5],
+	[0,	14,	7,	11,	10,	4,	13,	1,	5,	8,	12,	6,	9,	3,	2,	15],
+	[13,	8,	10,	1,	3,	15,	4,	2,	11,	6,	7,	12,	0,	5,	14,	9],
+    ],
+
+    [[10,	0,	9,	14,	6,	3,	15,	5,	1,	13,	12,	7,	11,	4,	2,	8],
+	[13,	7,	0,	9,	3,	4,	6,	10,	2,	8,	5,	14,	12,	11,	15,	1],
+    [13,	6,	4,	9,	8,	15,	3,	0,	11,	1,	2,	12,	5,	10,	14,	7],
+	[1,	10,	13,	0,	6,	9,	8,	7,	4,	15,	14,	3,	11,	5,	2,	12],
+    ],
+
+    [[7,	13	,14	,3	,0	,6	,9	,10	,1	,2	,8	,5	,11	,12	,4	,15],
+	[13,	8	,11	,5	,6	,15	,0	,3	,4	,7	,2	,12	,1	,10	,14	,9],
+	[10,	6	,9	,0	,12	,11	,7	,13	,15	,1	,3	,14	,5	,2	,8	,4],
+	[3,	15	,0	,6	,10	,1	,13	,8	,9	,4	,5	,11	,12	,7	,2	,14],
+    ],
+
+    [[2,	12	,4	,1	,7	,10	,11	,6	,8	,5	,3	,15	,13	,0	,14	,9],
+	[14,	11	,2	,12	,4	,7	,13	,1	,5	,0	,15	,10	,3	,9	,8	,6],
+	[4,	2 ,1	,11	,10	,13	,7	,8	,15	,9	,12	,5	,6	,3	,0	,14],
+	[11,	8	,12	,7	,1	,14	,2	,13	,6	,15	,0	,9	,10	,4	,5	,3],
+    ],
+
+    [[12,1	,10	,15	,9	,2	,6	,8	,0	,13	,3	,4	,14	,7	,5,	11],
+	[10,	15	,4	,2	,7	,12	,9	,5	,6	,1	,13	,14	,0	,11	,3	,8],
+	[9,	14	,15	,5	,2	,8	,12	,3	,7	,0	,4	,10	,1	,13	,11	,6],
+	[4,	3	,2	,12	,9	,5	,15	,10	,11	,14	,1	,7	,6	,0	,8	,13],
+    ],
+
+    [[4,	11	,2	,14	,15	,0	,8	,13	,3	,12	,9	,7	,5	,10	,6	,1],
+	[13,	0	,11	,7	,4	,9	,1	,10	,14	,3	,5	,12	,2	,15	,8	,6],
+	[1,	4	,11	,13	,12	,3	,7	,14	,10	,15	,6	,8	,0	,5	,9	,2],
+	[6,	11	,13	,8	,1	,4	,10	,7	,9	,5	,0	,15	,14	,2	,3	,12],
+    ],
+
+    [[13,	2	,8	,4	,6	,15	,11	,1	,10	,9	,3	,14	,5	,0	,12, 7],
+	[1,	15	,13	,8	,10	,3	,7	,4	,12	,5	,6	,11	,0	,14	,9	,2],
+	[7,	11	,4	,1	,9	,12	,14	,2	,0	,6	,10	,13	,15	,3	,5	,8],
+	[2,	1	,14	,7	,4	,10	,8	,13	,15	,12	,9	,0	,3	,5	,6	,11],
+    ]
+
+    # es un linko ayuda https://github.com/RobinDavid/pydes/blob/master/pydes.py#L42
+    
+
+
 ]
 
-def strToBits(input):
-	binary = "" #binary string to return
-	if len(input) < 8:#if len less than 8
-		input = input.ljust(8,'0')#Pad w/ zeros
-
-	for char in input:#get char in input
-		binVal = format(ord(char),'08b')#convert to number, than format to binary 8 digits long
-		binary += binVal#add binary for char into the binary string
-
-	return binary#return binary string to user
-
-def bitsToStr(input):
-	outputString = ""
-	#use chr here
 
 
-def initialPerm(binString, ip):
-	return [binString[x-1] for x in ip]#swap positions with initPermutation
-
-
-def expanFunc(binString, ef):
-	return [binString[x-1] for x in ef]#swap positions with initPermutation
 
 
 
 def encrypt(plaintext, key):
-	ciphertext = ""
-	return ciphertext
+    ciphertext = ""
+    return ciphertext
 
 def decrypt(ciphertext, key):
-	plaintext=""
-	return plaintext
+    plaintext=""
+    return plaintext
 
 def DES(number, key, method):
-	key = key[:1] + key[1:]
+    key = key[:1] + key[1:]
 
-def xor(key, rHalf):
-	result = ""
-	for num in range(len(str(key))):
-		result += str(key[num] ^ rHalf[num])
-	return result
+def strToBits(input):
+    binary = "" #binary string to return
+    if len(input) < 8:#if len less than 8
+        input = input.ljust(8,'0')#Pad w/ zeros
+
+    for char in input:#get char in input
+        binVal = format(ord(char),'08b')#convert to number, than format to binary 8 digits long
+        binary += binVal#add binary for char into the binary string
+
+    return binary#return binary string to user
+
+def initialPerm(binString, ip):
+    return [binString[x-1] for x in ip]#swap positions with initPermutation
+
+def expanFunc(binString, ef):
+    return [binString[x-1] for x in ef]#swap positions with initPermutation
 
 
 if __name__ == '__main__':
+    text = ""
+    print('DES Implementation:\n')
+    while text != "Quit":
+        text = str(input('Enter text to encrypt ("Exit" to quit): '))
+        
+        #see rng based on system time
+        random.seed(time.time())
+        
+        #get 56 bit key
+        key = random.getrandbits(56)
+        
+        #convert text to binary
+        binRep = strToBits(text)
+        
+        #perform initial permutation
+        initialPermutation = initialPerm(binRep, ip)
+        
+        #split permutation into 32 bit bits halves
+        lhalf, rhalf = initialPermutation[:len(initialPermutation)//2], initialPermutation[len(initialPermutation)//2:]
+        
+        #perform expansion permutation
+        rhalf = expanFunc(rhalf,ef)
+        
+        #convert back to integer
+        intRhalf = int(''.join(map(str,rhalf)),2)
+    
+        #xor two integers together
+        xord = key ^ intRhalf
 
-	text = ""
-	print('DES Implementation:\n')
-	while text != "Quit":
-		text = str(input('Enter text to encrypt ("Exit" to quit): '))
-		random.seed(time.time())
-		key = ""
-		for _ in range(56):
-			keyPart = random.randint(0,1)
-			key += str(keyPart)
-
-		intkey = int(key)
-		#print(intkey)
-		binRep = strToBits(text)
-		initialPermutation = initialPerm(binRep, ip)
-		lhalf, rhalf = initialPermutation[:len(initialPermutation)//2], initialPermutation[len(initialPermutation)//2:]
-		rhalf = expanFunc(rhalf,ef)
-
-		intRhalf = int(''.join(map(str,rhalf)))
-
-		xorRhalf = xor(intkey, intRhalf)
-
-
-		#xorRhalf =intRhalf ^ intkey
-		#print('{0:b}'.format(xorRhalf))
-
-
-		#xorRhalf= xor(intRhalf, intkey)
-		#xorRhalf = intRhalf ^ intkey 
-		#print(intRhalf)
-		#print(intkey)
-
+        #s-box
 
 
 
-
-
-		text = "Quit"
-		
-
-
-def split(text):
-	textlength = len(text/2)
-	return text[:textlength] + text[textlength:]
+        #xor the values
+  
 
 
 
-#for i in range(16):
-	#split into 32 bits halves
-	lhalf, rhalf = textInput[:len(textInput)//2], textInput[len(textInput)//2:]
-	expanFunc(rhalf,ef)
 
+
+
+
+
+
+
+        
 
 
